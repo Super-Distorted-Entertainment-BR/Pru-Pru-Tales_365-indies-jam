c8505e7caaae48e1db9be7d7779896ff8a27f82e
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-08-21 18:06:46 -0300

    Adicionadas ações de start e restart do jogo, e textos para título e game over nas cenas

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour {
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			SceneManager.LoadScene("5 - Game");
+		}
+	}
+}
