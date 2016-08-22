c7db311967feec05fabb1393f97b895fbfec246c
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-08-21 23:03:11 -0300

    Tentativa de fazer o boss atacar

---
+++ Assets/Prefabs/mago/MagoSensorController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagoSensorController : MonoBehaviour {
+
+	public GameObject mago;
+	public GameObject magoWaked;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (!magoWaked && other.gameObject.CompareTag("Player"))
+		{
+			mago.GetComponent<MagoController>().wake = true;
+		}
+	}
+}
